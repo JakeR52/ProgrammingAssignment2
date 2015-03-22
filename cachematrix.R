55e74ab361cb20ac3a2c890341b09fd7a4c6daf5
JakeR52 <jake.rayner@gmail.com>
2015-03-22 12:30:37 -0700

    Initial copy
    work in progress

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,4 @@
+## IF THIS COMMENT IS STILL HERE FOR PEER EVALUATION THEN I HAVE NOT FINISHED THIS PROGRAMMING ASSIGNMENT
 ## Put comments here that give an overall description of what your
 ## functions do
 
